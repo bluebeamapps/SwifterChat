0b4acb804ffdf3284b98e49f3cef1f182a50a4e4
brunoaguiar <brunoaguiar01989@gmail.com>
2016-01-01 17:44:09 -0300

    Toolbar in the chat details controller now moves up and down with the keyboard.

--- SwifterChat/Controllers/ChatDetailsController.swift
+++ SwifterChat/Controllers/ChatDetailsController.swift
@@ -9,27 +9,51 @@
 import Foundation
 import UIKit
 
-class ChatDetailsController : UITableViewController {
+class ChatDetailsController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    @IBOutlet var toolbar: UIToolbar!
+    
+    @IBOutlet var toolbarBottomConstraint: NSLayoutConstraint!
+    var toolbarConstraintDefaultValue: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
         tableView.estimatedRowHeight = 100
         
-        navigationController?.toolbarHidden = false
+        //Set up the toolbar.
+        toolbarConstraintDefaultValue = toolbarBottomConstraint.constant
+        
+        let textField = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 30))
+        textField.backgroundColor = UIColor.blackColor()
+        
+        let item = UIBarButtonItem(customView: textField)
+        
+        toolbar.items = [item]
+        
+        //Register for keyboard events.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+        
+        //Add gesture recognizer that will listen for taps on the TableView. Keyboard will then be dismissed when a tap is detected.
+        let tgc = UITapGestureRecognizer(target: self, action: "onTableViewTapped")
+        tableView.addGestureRecognizer(tgc)
     }
-    
+
     override func viewWillDisappear(animated: Bool) {
         super.viewWillAppear(animated)
         
         navigationController?.toolbarHidden = true
     }
     
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 6
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell: UITableViewCell!
         
         if indexPath.row % 2 == 0 {
@@ -51,4 +75,30 @@ class ChatDetailsController : UITableViewController {
         return cell
     }
     
+    func keyboardWillShow(notification: NSNotification) {
+        let info = notification.userInfo!
+        
+        let keyboardFrame = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let duration = info[UIKeyboardAnimationDurationUserInfoKey] as! Double
+        
+        UIView.animateWithDuration(duration) { () -> Void in
+            self.toolbarBottomConstraint.constant = keyboardFrame.size.height
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        let info = notification.userInfo!
+        
+        let duration = info[UIKeyboardAnimationDurationUserInfoKey] as! Double
+        
+        UIView.animateWithDuration(duration) { () -> Void in
+            self.toolbarBottomConstraint.constant = self.toolbarConstraintDefaultValue
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    func onTableViewTapped() {
+        self.view.endEditing(true)
+    }
 }
